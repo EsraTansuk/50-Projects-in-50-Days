8ad6ba0de6dd9f4bca24d90179b2ef1b04c1c6da
Esra Tansuk <esra.balkis@outlook.com>
2023-04-18 14:21:24 +0300

    vote and other js code was writed

--- day 18/movie-api/script.js
+++ day 18/movie-api/script.js
@@ -1,1 +1,73 @@
-const API_URL = ''
+const API_URL =
+  "https:/api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=bc923c158be3c81f78c5265aff165b16&page=1";
+
+const IMG_PATH = "https://image.tmdb.org/t/p/w500";
+
+const SEARCH_API =
+  'https:/api.themoviedb.org/3/search/movie?api_key=bc923c158be3c81f78c5265aff165b16&query="';
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+//Get initial movies
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `
+        <div class="movie">
+      <img
+        src="${IMG_PATH + poster_path}" />
+      <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="green">${vote_average}</span>
+      </div>
+      <div class="overview">
+        <h3>Overview</h3>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat, dolorem ratione quas incidunt nisi repellat
+        veritatis, modi similique blanditiis quibusdam minus, quasi porro exercitationem qui repudiandae officia
+        nesciunt est quidem?
+      </div>
+    </div>
+        `;
+  });
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green'
+    } else if(vote >= 5) {
+        return 'orange'
+    } else {
+        return('red')
+    }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.ariaValueMax;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
